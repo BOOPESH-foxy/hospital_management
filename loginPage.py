ccc5413a32dbf9d4ee89545108d5011f4e8cbae4
BOOPESH-foxy <boopesh.mc@gmail.com>
2023-06-10 10:03:37 +0530

    credential checking

--- loginPage.py
+++ loginPage.py
@@ -1,5 +1,6 @@
 from os import system, name
 import time
+
 class User():
     def __init__(self,userName,isAdmin,hospital_id=None,slotId=None,date=None) -> None:
         self.userName = userName
@@ -15,10 +16,14 @@ class startPage():
         self.hmdb = db
         
     def check_credentials(self,username,password):
-        pass
+        if (username.casefold() == user and password == passw):
+            print("logged in ....")
+            return 1
+        else:
+            return 0
     
     def credential_prompt(self):
-        Case = int(input("[1] : Login \n [2] : signUp \n Enter[1/2] : "))
+        Case = int(input("[1] : Login \n[2] : signUp\nEnter[1/2] : "))
         if (Case == 1):
             username = input("UserName : ")
             password = input("PassWord : ")
@@ -28,15 +33,17 @@ class startPage():
     def login_page(self):
             status = self.credential_prompt()
             if (status == 0):
-                caseRetry = ("Wanna give a try again [y/n] : ")
-                if (caseRetry is ['y' or 'Y']):
+                system('clear')
+                caseRetry = input("Failed !! \n Wanna give a try again [y/n] : ")
+                if (caseRetry =='y' or 'Y'):
+                    system('clear')
                     self.credential_prompt()
                 else:
                     system('clear')
                     print("quitting .. ")
-                    exit()
             else:
-                
+                pass
             
-if __name__  == "__main__":
-    startPage.login_page()
+# if __name__  == "__main__":
+#     ClassObject = startPage()
+#     ClassObject.login_page()
