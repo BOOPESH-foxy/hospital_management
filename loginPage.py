6999291ab58df010c7b39c4d36b3749d6bfc6e7f
BOOPESH-foxy <boopesh.mc@gmail.com>
2023-06-12 10:17:38 +0530

    checking user status and to print whether admin or user

--- loginPage.py
+++ loginPage.py
@@ -21,18 +21,24 @@ class startPage():
     def check_credentials(self,username,password):
         selectQuery = "select isadmin from authentication where username like %s and password like %s"
         data = (username,password)
-        print(self.hmcursor.execute(selectQuery,data))
+        self.hmcursor.execute(selectQuery,data)
+        isUser = hmcursor.fetchall()
+        if(isUser):
+            return isUser
+        else:
+            return 0
         
     def credential_prompt(self):
         Case = int(input("[1] : Login \n[2] : signUp\nEnter[1/2] : "))
         if (Case == 1):
-            username = str(input("UserName : "))
+            self.username = str(input("UserName : "))
             password = pwinput.pwinput(prompt="PassWord : ",mask="*")
-            result = self.check_credentials(username, password)
+            result = self.check_credentials(self.username, password)
             return result
         
     def login_page(self):
             status = self.credential_prompt()
+            print("status :",status[0][0])
             if (status == 0):
                 system('clear')
                 caseRetry = input("Failed !! \n Wanna give a try again [y/n] : ")
@@ -42,9 +48,13 @@ class startPage():
                 else:
                     system('clear')
                     print("quitting .. ")
-            else:
-                pass
-            
+            elif(status[0][0] == True):
+                system('clear')
+                print("WELCOME ADMIN {0}!".format(self.username).capitalize().upper())
+                
+            elif(status[0][0] == False):
+                system('clear')
+                print("welcome user {0} !".format(self.username).capitalize().upper())
 if __name__  == "__main__":
 
     hmDb = psycopg2.connect(
